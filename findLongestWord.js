f89d8a83a89a44ff6a9743abdf2a1be13972c1e9
Mohammad Ali <mohammadaliqureshi7610@gmail.com>
2023-07-19 21:16:01 +0530

    first commit

---
+++ findLongestWord.js
@@ -0,0 +1,17 @@
+function findLongestWord(str) {
+  let words = str.split(" ");
+  let longestWord = "";
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i].replace(/[^a-zA-Z]/g, "");
+
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+
+  return longestWord;
+}
+
+let sentence = "My name is ankit jain & i'm from morena";
+console.log("Longest word:", findLongestWord(sentence));
