0482d562512fc8626984976b670b309c0fc5ca3e
Mohammad Ali <mohammadaliqureshi7610@gmail.com>
2023-08-08 20:40:38 +0530

    some change

--- arrengeTheNumbs.js
+++ arrengeTheNumbs.js
@@ -1,6 +1,6 @@
 // step - 1 = creat a function arrangeTheNums with parameter arr
-// step - 2 = consider a variable zeros with empty string
-// step - 3 = consider a another variable ones with empty string
+// step - 2 = consider a variable zeros with empty array
+// step - 3 = consider a another variable ones with empty array
 // step - 4 = Run a loop with i is equal to zero and i is less then arr.length and i++
 // step - 5 = In the loop apply a if condition if arr[i] is equal to or type of '0' then store the 0 in the zeros variable and adding another 0
 // step - 6 = Also apply a else if condition and esle if arr[i] is equal to or type of '1' then store the 0 in the ones variable and adding another 1
@@ -11,8 +11,8 @@
 const original = '111000111100011100';
 
 function arrangeTheNums(arr) {
-    let zeros = '';
-    let ones = '';
+    let zeros = [];
+    let ones = [];
   
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] === '0') {
