de432031270210922f303bb915e97596635f9077
Mohammad Ali <mohammadaliqureshi7610@gmail.com>
2023-08-08 21:21:06 +0530

    solve with another aproch

--- arrengeTheNumbs.js
+++ arrengeTheNumbs.js
@@ -1,30 +1,29 @@
 // step - 1 = creat a function arrangeTheNums with parameter arr
-// step - 2 = consider a variable zeros with empty array
-// step - 3 = consider a another variable ones with empty array
-// step - 4 = Run a loop with i is equal to zero and i is less then arr.length and i++
-// step - 5 = In the loop apply a if condition if arr[i] is equal to or type of '0' then store the 0 in the zeros variable and adding another 0
-// step - 6 = Also apply a else if condition and esle if arr[i] is equal to or type of '1' then store the 0 in the ones variable and adding another 1
-// step - 7 = return the function
-// step - 8 = call the function in consol.log with original 
+// step - 2 = consider a variable zeros with  value is 0
+// step - 3 = Run a loop with i is equal to zero and i is less then arr.length and i++
+// step - 4 = In the loop apply a if condition if arr[i] is equal to or type of '0' then consider a varaible temp is eqaul to arr [i] then apply  arr[i] is eqaul to arr[zero] then apply arr[zero] is eqaul to temp the then zero++
+// step - 5 = return the arr.join('')
+// step - 6 = change the string in array with apply .split('')
+// step - 7 = call the function in consol.log with original 
 
 
-const original = '111000111100011100';
+
 
 function arrangeTheNums(arr) {
-    let zeros = [];
-    let ones = [];
+  let zero = 0;
   
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === '0') {
-        zeros += '0';
-      } else if (arr[i] === '1') {
-        ones += '1';
-      }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '0') {
+      const temp = arr[i];
+      arr[i] = arr[zero];
+      arr[zero] = temp;
+      zero++;
     }
-  
-    return zeros + ones;
   }
-  
-  
-  console.log('Original Binary:', original);
-  console.log('Rearranged Binary:', arrangeTheNums(original) );
+
+  return arr.join('');
+}
+
+const original = '111000111100011100'.split('');
+
+console.log('Rearranged :', arrangeTheNums(original));
